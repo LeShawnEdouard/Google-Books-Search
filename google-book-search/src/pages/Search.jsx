2a480bb4dc91b42d9a9c65cf769de219b387fcf1
LeShawn Edouard <leshawnmedouard@gmail.com>
2019-08-04 18:49:55 -0400

    Imported navtab into search page

---
+++ google-book-search/src/pages/Search.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Nav from "../components/NavTabs/index";
+
+class Search extends Component {
+  render() {
+    return (
+      <div>
+        <Nav></Nav>
+      </div>
+    )
+  }
+}
+
+export default Search;
