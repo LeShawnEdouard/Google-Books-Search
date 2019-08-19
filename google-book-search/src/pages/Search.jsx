52a3b05714f96b86d71dc444d9b18d189a686b34
LeShawn Edouard <leshawnmedouard@gmail.com>
2019-08-19 19:26:50 -0400

    Added Jumbotron for search box

--- google-book-search/src/pages/Search.jsx
+++ google-book-search/src/pages/Search.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
-import Hero from '../components/Hero';
+import Hero from "../components/Hero";
+import Jumbotron from "../components/Jumbotron";
 
 
 class Search extends Component {
   render() {
     return (
       <div>
-        <Hero backgroundImage="https://images.unsplash.com/photo-1558210834-473f430c09ac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" />
+        <Hero backgroundImage="https://images.unsplash.com/photo-1558210834-473f430c09ac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80">
+        <p>Test</p>
+        </Hero>
+        <Jumbotron />
       </div>
     )
   }
