d863e2b0a3c6312efca38d56b36e0c44e05f6f94
LeShawn Edouard <leshawnmedouard@gmail.com>
2020-07-19 21:22:50 -0400

    Replaced Jumbotron with container for the 'saved books' page

--- google-book-search/src/pages/Saved.jsx
+++ google-book-search/src/pages/Saved.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import Hero from '../components/Hero';
+import SavedContainer from "../components/Saved";
 
 
 class Saved extends Component {
   render() {
     return (
       <div>
-        <Hero backgroundImage="https://images.unsplash.com/photo-1558210834-473f430c09ac?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" />
+        <Hero backgroundImage="https://images.unsplash.com/photo-1526721940322-10fb6e3ae94a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80">
+        </Hero>
+        <SavedContainer />
+        
       </div>
     )
   }
