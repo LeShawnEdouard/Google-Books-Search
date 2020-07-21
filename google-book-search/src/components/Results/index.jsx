2b990219cb44746e164105c812439dd2c9ae92e9
LeShawn Edouard <leshawnmedouard@gmail.com>
2020-07-21 12:37:16 -0400

    Added 'view' and 'save' buttons to each card

--- google-book-search/src/components/Results/index.jsx
+++ google-book-search/src/components/Results/index.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 function Results() {
     return (
@@ -12,6 +15,12 @@ function Results() {
     <Card.Img variant="top" src="holder.js/100px160" />
     <Card.Body>
       <Card.Title>Card title</Card.Title>
+        <Row>
+          <Col>
+            <Button variant="primary">View</Button>
+            <Button variant="primary">Save</Button>
+          </Col>
+        </Row>
       <Card.Text>
         This is a wider card with supporting text below as a natural lead-in to
         additional content. This content is a little bit longer.
@@ -25,6 +34,12 @@ function Results() {
     <Card.Img variant="top" src="holder.js/100px160" />
     <Card.Body>
       <Card.Title>Card title</Card.Title>
+        <Row>
+          <Col>
+            <Button variant="primary">View</Button>
+            <Button variant="primary">Save</Button>
+          </Col>
+        </Row>
       <Card.Text>
         This card has supporting text below as a natural lead-in to additional
         content.{' '}
@@ -38,6 +53,12 @@ function Results() {
     <Card.Img variant="top" src="holder.js/100px160" />
     <Card.Body>
       <Card.Title>Card title</Card.Title>
+        <Row>
+          <Col>
+            <Button variant="primary">View</Button>
+            <Button variant="primary">Save</Button>
+          </Col>
+        </Row>
       <Card.Text>
         This is a wider card with supporting text below as a natural lead-in to
         additional content. This card has even longer content than the first to
